e6bca51183c19999e7b9b1a5a8b860fb1c41c6f2
leoberson <leobersonlopes@hotmail.com>
2019-05-18 10:04:00 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/leoberson/cursomc/domain/Produto.java
+++ src/main/java/com/leoberson/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.leoberson.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -25,12 +28,18 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	
 	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",         
 		joinColumns = @JoinColumn(name = "produto_id"), 
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")     
 	)  
+	
+
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	public Produto() {
@@ -44,6 +53,14 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	public List<Pedido> getPedidos(){
+		List<Pedido> list = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			list.add(x.getPedido());
+		}
+		return list;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -76,6 +93,13 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
