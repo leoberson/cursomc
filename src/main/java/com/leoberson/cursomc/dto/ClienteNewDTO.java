c5fdcebbd87d79d33e2b05dae240ca1c2d6c0c2e
leoberson <lopesx22@gmail.com>
2020-01-29 16:21:29 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

--- src/main/java/com/leoberson/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/leoberson/cursomc/dto/ClienteNewDTO.java
@@ -5,25 +5,39 @@
  */
 package com.leoberson.cursomc.dto;
 
+import com.leoberson.cursomc.services.validation.ClienteInsert;
 import java.io.Serializable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 
 /**
  *
  * @author leoberson
  */
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @NotEmpty(message = "preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "o tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+    @NotEmpty(message = "preenchimento obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+    @NotEmpty(message = "preenchimento obrigatório")
     private String cpfOuCnpj;
     private Integer tipo;
+    @NotEmpty(message = "preenchimento obrigatório")
     private String logradouro;
+    @NotEmpty(message = "preenchimento obrigatório")
     private String numero;
     private String complemento;
-    private String bairro;
-    private String cep;
     
+    private String bairro;
+    @NotEmpty(message = "preenchimento obrigatório")
+    private String cep;
+    @NotEmpty(message = "preenchimento obrigatório")
     private String telefone1;
     private String telefone2;
     private String telefone3;
---
+++ src/main/java/com/leoberson/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leoberson.cursomc.services.validation;
+
+import com.leoberson.cursomc.domain.enums.TipoCliente;
+import com.leoberson.cursomc.dto.ClienteNewDTO;
+import com.leoberson.cursomc.resources.exceptions.FieldMessage;
+import com.leoberson.cursomc.services.validation.utils.BR;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())
+                && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+        
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
+                && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        // inclua os testes aqui, inserindo erros na lista
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
